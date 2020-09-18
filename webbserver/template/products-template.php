1c27fc9a5ace6f52e0634d0132b3607b16cd9af0
EddieVestman <33714818+EddieVestman@users.noreply.github.com>
2020-09-18 11:00:47 +0200

    Add files via upload

--- webbserver/template/products-template.php
+++ webbserver/template/products-template.php
@@ -11,6 +11,7 @@
       <?php
 		require "masthead.php";
 		require "menu.php";
+		require "../html/varor.php";
 		
 		?>	
 		
@@ -30,34 +31,23 @@
 						</tr>
 					</thead>
 					<tbody>
-						<tr>
-							<td>Äpple</td>
-							<td>Grönt surt</td>
-							<td><img src="bilder/apple.jpg" alt="Grönt surt"></td>
-							<td>50</td>
-							<td><a href="#">Köp</a></td>
-						</tr>
-						<tr>
-							<td>Apelsin</td>
-							<td>Orange söt</td>
-							<td><img src="bilder/orange.jpg" alt="Orange söt"></td>
-							<td>38</td>
-							<td><a href="#">Köp</a></td>
-						</tr>
-						<tr>
-							<td>Päron</td>
-							<td>Gult saftigt</td>
-							<td><img src="bilder/pear.jpg" alt="Gult saftigt"></td>
-							<td>100</td>
-							<td><a href="#">Köp</a></td>
-						</tr>
-						<tr>
-							<td>Banan</td>
-							<td>Gul böjd</td>
-							<td><img src="bilder/banana.jpg" alt="Gul böjd"></td>
-							<td>30</td>
-							<td><a href="#">Köp</a></td>
-						</tr>
+					 <?php
+			   		 foreach($varor as $vara){
+						echo "<tr><td>";
+						echo $vara[0];
+						echo "</td><td>";
+					    echo $vara[1];
+						echo "</td><td><img src='";
+					    echo $vara[3];
+						echo "'alt='";
+						echo $vara[1];
+						echo "'></td><td>";
+						echo $vara[2];
+						echo "</td></tr>";
+
+
+					 }
+					?>	
 					</tbody>
 				</table>
 
--- webbserver/template/sida3-template.php
+++ webbserver/template/sida3-template.php
@@ -10,41 +10,27 @@
 			<?php
 		require "masthead.php";
 		require "menu.php";
+		require "varor.php";
 		
 		?>	
 			
 			<main> <!--Huvudinnehåll-->
 				<section id="content">
 					<h2>Varor</h2>
-          
-            <figure><img src="bilder/apple.jpg" alt="Grönt surt">
-                <figcaption>Äpple 50
-                  <p>
-                    <a href="#">Köp</a>
-                  </p>
-                </figcation>
-            </figure>
-            <figure><img src="bilder/orange.jpg" alt="Orange söt">
-                <figcaption>Apelsin 38
-                  <p>
-                    <a href="#">Köp</a>
-                  </p>
-                </figcation>
-            </figure>
-            <figure><img src="bilder/pear.jpg" alt="Gult saftigt">
-                <figcaption>Päron 100
-                  <p>
-                    <a href="#">Köp</a>
-                  </p>
-                </figcation>
-            </figure>
-            <figure><img src="bilder/banana.jpg" alt="Gul böjd">
-                <figcaption>Banan 30
-                  <p>
-                    <a href="#">Köp</a>
-                  </p>
-                </figcation>
-            </figure>
+					
+				<?php
+				 foreach($varor as $vara){
+				 echo <<<FIGURE
+				 <figure><img src="$vara[3]" alt="$vara[1]">
+				 <figcaption>$vara[0] $vara[2]
+				 <p>
+				 <a href="#">Köp</a>
+				 </p>
+				 </figcaption>
+				 </figure>
+FIGURE;
+				 }
+				?>
             
 			</section>
 				
