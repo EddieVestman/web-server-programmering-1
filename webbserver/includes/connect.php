03430bb4864537cc6cb3c16b5f76ba09cb4594b6
EddieVestman <33714818+EddieVestman@users.noreply.github.com>
2020-10-16 11:15:32 +0200

    Add files via upload

--- webbserver/includes/connect.php
+++ webbserver/includes/connect.php
@@ -17,13 +17,93 @@
 		exit;
 	}
 	
-	$dbh->close();
-	//var_dump($result);
 	
+	echo "<table><tr><th>Name</th>
+			<th>Descrpition</th>
+			<th>Price</th>
+			<th>Picture</th>
+			</tr>";
 	while($row = $result->fetch_assoc()){
+	echo "<tr><td>";
 	echo $row['name'];
-	echo "<br>";
-	echo $row ['price']," kronor";
+	echo "</td><td>";
+	echo $row['description'];
+	echo "</td><td>";
+	echo $row['price'];
+	echo "</td><td>";
+	echo $row ['picture'];
+	echo "</td></tr>";
+	
+	}
+	echo "</table>";
+	
+	echo "<br><br>";
+	
+	$sql = "SELECT users.username, users.email, customers.username, customers.firstname, customers.lastname FROM users
+	JOIN customers WHERE users.username = customers.username";
+	$res=$dbh->prepare($sql);
+	$res->execute();
+	$result=$res->get_result();
+	
+	if(!$result){
+		echo "Felaktig SQL fråga";
+		exit;
 	}
 	
+	echo "<table><tr><th>Anv namn</th>
+			<th>Förnamn</th>
+			<th>Efternamn</th>
+			<th>Email</th>
+			</tr>";
+	while($row = $result->fetch_assoc()){
+	echo "<tr><td>";
+	echo $row['username'];
+	echo "</td><td>";
+	echo $row['firstname'];
+	echo "</td><td>";
+	echo $row['lastname'];
+	echo "</td><td>";
+	echo $row ['email'];
+	echo "</td></tr>";
+	
+	}
+	echo "</table>";
+	
+			echo "<br>";
+	
+		$sql = "SELECT customers.adress, customers.zip, customers.city, customers.phone, customers.username FROM customers";
+	
+	$res=$dbh->prepare($sql);
+	$res->execute();
+	$result=$res->get_result();
+	
+	if(!$result){
+		echo "Felaktig SQL fråga";
+		exit;
+	}
+	
+	echo 	"<table><tr>
+			<th>Användare</th>
+			<th>Adress</th>
+			<th>Zip</th>
+			<th>City</th>
+			<th>Phone</th>
+			</tr>";
+	while($row = $result->fetch_assoc()){
+	echo "<tr><td>";
+	echo $row['username'];
+	echo "</td><td>";
+	echo $row['adress'];
+	echo "</td><td>";
+	echo $row['zip'];
+	echo "</td><td>";
+	echo $row['city'];
+	echo "</td><td>";
+	echo $row ['phone'];
+	echo "</td></tr>";
+	
+	}
+	echo "</table>";
+		//$dbh->close();
+
 ?>
