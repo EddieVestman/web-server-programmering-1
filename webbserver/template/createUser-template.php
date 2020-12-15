9eb80fe5fd325515970d5428621bc6ac1030c915
EddieVestman <33714818+EddieVestman@users.noreply.github.com>
2020-12-15 11:01:05 +0100

    Update createUser-template.php

--- webbserver/template/createUser-template.php
+++ webbserver/template/createUser-template.php
@@ -1,59 +1,89 @@
 <?php
- if(isset($_POST['fnamn'] )&& isset($_POST['enamn'])&&isset $_POST['mail']) &&isset $_POST['adress']) && isset$_POST['zip'])&& isset$_POST['ort'])&& isset$_POST['nummer']) ))
-$fnamn = filter_input(INPUT_POST,'fname', FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_LOW);
-$enamn = filter_input(INPUT_POST,'enamn', FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_LOW);
-$mail = filter_input(INPUT_POST,'mail', FILTER_SANITIZE_EMAIL, FILTER_FLAG_STRIP_LOW);
-$adress = filter_input(INPUT_POST,'adress', FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_LOW);
-$zip = filter_input(INPUT_POST,'zip', FILTER_SANITIZE_NUMBER_INT);
-$ort = filter_input(INPUT_POST,'ort', FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_LOW);
-$nummer = filter_input(INPUT_POST,'nummer', FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_LOW);
-
-
- $sql="SELECT * FROM users WHERE username = ? OR email = ?";
- $res=$dbh->prepare($sql);
- $res->bind_param("ss",$username, $mail);
- $res->execute();
- $result=$res->get_result();
- $row=$resuly->fetch_assoc();
- 
- require "../includes/connect.php";
- 
- 
- if($row !== NULL)
- {
-	 if($row['username']=== $username){
-		 header("location:createUser.php?name=$username");
-	 }
-	 
-	 elseif($row['email'] === $mail){
-		 header("location:createUser.php?mail=$mail");
-	 }
- }
- 
- $str="";
- 
- if(isset($_GET['name'])){
-	 $usr=$_GET['name'];
-	 $str="Användarnamnet $usr är upptaget";
- }
- elseif(isset($_GET['mail'])){
-	$ma=$_GET['mail'];
-	$str="Mailadressen $ma är upptagen";
- }
+$str="";
+	if(isset($_POST['fname'])&& isset($_POST['ename'] )&& isset( $_POST['mail'] )&& isset($_POST ['adress']) && isset( $_POST ['zip']) && isset ($_POST ['ort']) && isset ( $_POST ['nummer']) && isset ($_POST ['aname']) && isset ($_POST ['password'])) 
+	{
+		$fnamn = filter_input(INPUT_POST,'fname', FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_LOW);
+		$enamn = filter_input(INPUT_POST,'ename', FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_LOW);
+		$mail = filter_input(INPUT_POST,'mail', FILTER_SANITIZE_EMAIL, FILTER_FLAG_STRIP_LOW);
+		$adress = filter_input(INPUT_POST,'adress', FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_LOW);
+		$zip = filter_input(INPUT_POST,'zip', FILTER_SANITIZE_NUMBER_INT);
+		$ort =  filter_input(INPUT_POST,'ort', FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_LOW);
+		$nummer = filter_input(INPUT_POST,'nummer', FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_LOW);
+		$aname = filter_input(INPUT_POST,'aname', FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_LOW);
+		$password = filter_input(INPUT_POST,'password', FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_LOW);
+			
+		require "../includes/connect.php";
+			
+		$sql="SELECT * FROM users WHERE username = ? OR email = ?";
+		$res=$dbh->prepare($sql);
+		$res->bind_param("ss",$username, $mail);
+		$res->execute();
+		$result=$res->get_result();
+		$row=$result->fetch_assoc();
+		
+		if($row !== NULL)
+		{
+			if($row['username']=== $username) {
+				header("location:createUser.php?name=$username");
+			}
+			elseif($row['email'] === $mail) {
+				header("location;createUser.php?mail=$mail");
+			}
+		}
+		$str="";
+		
+		if (isset($_GET['aname'])) {
+			$usr=$_GET['aname'];
+			$str="Användarnamnet $usr upptaget";
+		}
+		elseif(isset($_GET['mail'])) {
+				$ma=$_GET['mail'];
+				$str="Mailadressen $ma är upptagen";
+		}
+		else
+		{
+			$status = 1;
+			$sql = "INSERT INTO users(username, email, password, status) VALUE (?,?,?,?)";
+			$res=$dbh->prepare($sql);
+			$res->bind_param("sssi",$username, $email, $password, $status);
+			$res->execute();
+			
+			$sql = "INSERT INTO customers(username, firstname, surname, adress, zip, city, phone) VALUE (?,?,?,?,?,?,?)";
+			$res=$dbh->prepare($sql);
+			$res->bind_param("ssssiss",$username, $firstname, $surname, $adress, $zip, $city, $phone);
+			$res->execute();
+			$str="Användaren tillagd";
+		}
+	}
 	else
 	{
-		$staus =1;
-		$sql="INSERT INTO users(username, email, passowrd, status) VALUE (?,?,?,?)";
-		$res=$dbh->prepare($sql);
-		$res->bind_param("sssi",$username, $mail, $password, $status);
-		$res->execute();
-		
-		$sql = "INSERT INTO customers(username, firstname, lastname, adress, zip, city, phone) VALUE (?,?,?,?,?,?,?)";
-		$res=$dbh->prepare($sql);
+		$str.=<<<FORM
+		<form action="createUser.php" method="post">
+            <p><label for="fnamn">Förnamn:</label>
+            <input type="text" id="fname" name="fname"></p>
+			<p><label for="enamn">Efternamn:</label>
+			<input type="text" id="ename" name="ename"></p>
+			<p><label for="mail">Epost:</label>
+			<input type="email" id="mail" name="mail"></p>
+			<p><label for="adress">Adress:</label>
+			<input type="text" id="adress" name="adress"></p>
+			<p><label for="zip">Postnummer:</label>
+			<input type="text" id="zip" name="zip"></p>
+			<p><label for="ort">Postort:</label>
+			<input type="text" id="ort" name="ort"></p>
+			<p><label for="nummer">Telefon:</label>
+			<input type="text" id="nummer" name="nummer"></p>
+			<p><label for="anamn">Användarnamn:</label>
+            <input type="text" id="aname" name="aname"></p>
+            <p><label for="pwd">Lösenord:</label>
+            <input type="password" id="pwd" name="password"></p>
+            <p>
+            <input type="submit" value="Skapa användare">
+            </p>
+          </form>
+FORM;
 	}
-
 ?>
-
 
 <!DOCTYPE html>
 
@@ -77,32 +107,11 @@ $nummer = filter_input(INPUT_POST,'nummer', FILTER_SANITIZE_STRING, FILTER_FLAG_
 		
 			<main> <!--Huvudinnehåll-->
 				<section>
-					<?php echo $str; ?> 
-						 
-			<form action="login2.php" method="post">
-            <p><label for="fnamn">Förnamn:</label>
-            <input type="text" id="fname" name="fnamn"></p>
-			<p><label for="enamn">Efternamn:</label>
-			<input type="text" id="ename" name="ename"></p>
-			<p><label for="mail">Epost:</label>
-			<input type="email" id="mail" name="mail"></p>
-			<p><label for="adress">Adress:</label>
-			<input type="text" id="adress" name="adress"></p>
-			<p><label for="zip">Postnummer:</label>
-			<input type="text" id="zip" name="zip"></p>
-			<p><label for="ort">Postort:</label>
-			<input type="text" id="ort" name="ort"></p>
-			<p><label for="nummer">Telefon:</label>
-			<input type="text" id="nummer" name="nummer"></p>
-            <p><label for="pwd">Lösenord:</label>
-            <input type="password" id="pwd" name="password"></p>
-            <p>
-            <input type="submit" value="Skapa användare">
-            </p>
-          </form>
+					<?php 
+						echo "$str"; 
+					?>
 				</section>
 			</main>
-
     </div>
     <?php	
 		require "footer.php";
